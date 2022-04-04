f21a0d8ef515e5062a2a02c8e951a1b6ab0d1da5
Thich Cay Developer Team <support@thichcay.com>
2022-04-05 01:42:34 +0700

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -43,7 +43,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { seller: 'seller', label: 'Hướng dẫn' },
+    { doc: 'seller', label: 'Hướng dẫn' },
     { doc: 'doc2', label: 'Chính sách' },
     { page: 'help', label: 'Liên hệ' },
     { blog: true, label: 'Tham khảo' },
