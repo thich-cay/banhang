2f453a9d1dd77176cfbfeb7ffc5ec9a72bf45dc8
Thich Cay Developer Team <support@thichcay.com>
2022-04-05 02:14:43 +0700

    Update Footer.js

--- core/Footer.js
+++ core/Footer.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022, Thich Am Thuc LLC.
+ * Copyright (c) 2022 Thich Am Thuc LLC.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
